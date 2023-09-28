16d3e8ffe8fbe878c2e49928df74e492a3881b1f
Nishant Banginwar <banginwar.nishant1@gmail.com>
2023-09-28 20:46:52 +0530

    third

---
+++ guessinggame.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Function to count the files in the current directory
+count_files() {
+    local file_count=$(ls | wc -l)
+    echo "$file_count"
+}
+
+# Main game loop
+while true; do
+    correct_count=$(count_files)
+    echo "How many files are in the current directory?"
+    read user_guess
+
+    if [[ $user_guess -lt $correct_count ]]; then
+        echo "Too low! Try again."
+    elif [[ $user_guess -gt $correct_count ]]; then
+        echo "Too high! Try again."
+    else
+        echo "Congratulations! You guessed correctly. There are $correct_count files."
+        break
+    fi
+done
